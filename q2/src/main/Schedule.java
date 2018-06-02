08fd9cc3f8097acf2950e92fb622cf029972b3a8
aisr <amandawng2011@gmail.com>
2018-06-02 14:15:30 -0700

    change to real variables

--- q2/src/main/Schedule.java
+++ q2/src/main/Schedule.java
@@ -5,47 +5,51 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Schedule{
-    static final String SOLUTION = "solution";
-    static final String FAILURE = "failure";
+    private static final String SOLUTION = "solution";
+    private static final String FAILURE = "failure";
 
-    List<String> varables;
-    Integer[] domain;
+    private int a = 0;
+    private int b = 1;
+    private int c = 2;
+    private int d = 3;
+    private int e = 4;
+    private int f = 5;
+    private int g = 6;
+    private int h = 7;
+
+    private List<String> varables;
+    private Integer[] domain;
 
     private void init(){
-//        varables = new ArrayList<>(
-//                Arrays.asList("A","B","C","D","E","F","G","H"));
-//        domain = new Integer[]{1,2,3,4};
         varables = new ArrayList<>(
-                Arrays.asList("X","Y","Z"));
-        domain = new Integer[]{1,2};
+                Arrays.asList("A","B","C","D","E","F","G","H"));
+        domain = new Integer[]{1,2,3,4};
         buildTree(new Tree(null, null), varables, new ArrayList<>(), 0);
     }
 
 
     private Tree buildTree(Tree node, List<String> varibles, List<Integer> preValues, int depth){
         if(depth >= varibles.size() ){
-            System.out.println(SOLUTION + "; depth:" + depth + "; size" + varibles.size());
+            System.out.println(SOLUTION);
             return null;
         }
 
         String var = varibles.get(depth);
-        depth++;
-        int varIndex = depth - 1;
 
         for(Integer value : domain) {
             System.out.print(var + " = " + value + " ");
 
-            if(varIndex < preValues.size()) {
-                preValues.set(varIndex, value);
+            if(depth < preValues.size()) {
+                preValues.set(depth, value);
             }else{
                 preValues.add(value);
             }
 
             if(checkConditionsFail(preValues, depth)) {
-                System.out.println(FAILURE + "  " + depth);
+                System.out.println(FAILURE);
             }else{
 
-                node.addChild(buildTree(new Tree(var, value), varibles, preValues, depth));
+                node.addChild(buildTree(new Tree(var, value), varibles, preValues, depth + 1));
             }
         }
 
@@ -53,48 +57,24 @@ public class Schedule{
     }
 
     private boolean checkConditionsFail(List<Integer> values, int index){
-//        System.out.println("size = " + values.size()+ ";index = " + index);
-        index--;
-//        int a = 0;
-//        int b = 1;
-//        int c = 2;
-//        int d = 3;
-//        int e = 4;
-//        int f = 5;
-//        int g = 6;
-//        int h = 7;
 
-        int x=0;
-        int y=1;
-        int z=2;
-
-        if(index >=1 && (values.get(x) == values.get(y))) {
-            return true;
-        }
-
-        if(index >=2 && (values.get(y) == values.get(z))){
-            return true;
-        }
-
-        return false;
-
-//        return (index >=g && !(values.get(a) > values.get(g))) ||
-//                (index >=h && !(values.get(a) <= values.get(h))) ||
-//                (index >=f && !(Math.abs(values.get(f) - values.get(b)) == 1)) ||
-//                (index >=h && !(values.get(g)< values.get(h))) ||
-//                (index >=g && !(Math.abs(values.get(g)- values.get(c)) == 1)) ||
-//                (index >=h && !((Math.abs(values.get(h) - values.get(c)) % 2) == 0)) ||
-//                (index >=h && !(values.get(h) != values.get(d))) ||
-//                (index >=g && !(values.get(d)> values.get(g))) ||
-//                (index >=d && !(values.get(d)!= values.get(c))) ||
-//                (index >=e && !(values.get(e)!= values.get(c))) ||
-//                (index >=e && !(values.get(e)< (values.get(d)- 1))) ||
-//                (index >=h && !(values.get(e)!= (values.get(h) - 2))) ||
-//                (index >=g && !(values.get(g)!= values.get(f))) ||
-//                (index >=h && !(values.get(h) != values.get(f))) ||
-//                (index >=f && !(values.get(c) != values.get(f))) ||
-//                (index >=f && !(values.get(d)!= values.get(f))) ||
-//                (index >=f && !((Math.abs(values.get(e)- values.get(f)) % 2) == 1));
+        return (index >=g && !(values.get(a) > values.get(g))) ||
+                (index >=h && !(values.get(a) <= values.get(h))) ||
+                (index >=f && !(Math.abs(values.get(f) - values.get(b)) == 1)) ||
+                (index >=h && !(values.get(g)< values.get(h))) ||
+                (index >=g && !(Math.abs(values.get(g)- values.get(c)) == 1)) ||
+                (index >=h && !((Math.abs(values.get(h) - values.get(c)) % 2) == 0)) ||
+                (index >=h && !(values.get(h) != values.get(d))) ||
+                (index >=g && !(values.get(d)> values.get(g))) ||
+                (index >=d && !(values.get(d)!= values.get(c))) ||
+                (index >=e && !(values.get(e)!= values.get(c))) ||
+                (index >=e && !(values.get(e)< (values.get(d)- 1))) ||
+                (index >=h && !(values.get(e)!= (values.get(h) - 2))) ||
+                (index >=g && !(values.get(g)!= values.get(f))) ||
+                (index >=h && !(values.get(h) != values.get(f))) ||
+                (index >=f && !(values.get(c) != values.get(f))) ||
+                (index >=f && !(values.get(d)!= values.get(f))) ||
+                (index >=f && !((Math.abs(values.get(e)- values.get(f)) % 2) == 1));
     }
 
     public static void main(String[] args){
